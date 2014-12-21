9df8075877e3ad180294a6cdd1522feb52814445
umo2000 <camperumo@gmail.com>
2014-12-21 11:44:08 +0000

    Umo Version of Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script will provide you with the inverse of a square matrix
+## which is stored in the cache of system to avoid using up memory
 
-## Write a short comment describing this function
+## This function will take in your square matrix x, set the inverse to null,
+## write over an existing inverse with null if passing in a new matrix,
+## then returns the value of matrix that was passed in, 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv<- NULL    
+    set <- function(y){
+        x<<-y
+        inv<<-NULL
+    }
+    get<-function()x  
+    setinv <- function(solve) inv<<-solve 
+    getinv <- function() inv
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    inv<-x$getinv()
+    if(!is.null(inv)){
+        message("getting cached data")
+        return(inv)
+    }
+    data<-x$get()
+    inv<-solve(data,...)
+    x$setinv(inv)
+    inv
 }
